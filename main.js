fe085aed74da6f820421c6afbfbf75cd6766852e
Eryck Assis <eng.assis.dev@gmail.com>
2025-09-02 11:18:29 -0300

    Adiciona evento de mouseout para alternar submenu no cabeçalho

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
   item.addEventListener("mouseover", () => alternarSubmenu(item, true));
+  item.addEventListener("mouseout", () => alternarSubmenu(item, false));
 });
